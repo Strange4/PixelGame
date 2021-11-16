9ec2c5377d7e6cc2add7aa709249d0d14a261831
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-11-16 14:44:02 -0500

    null pointer exception from last push, forgot to build

--- src/game/entity/Entity.java
+++ src/game/entity/Entity.java
@@ -9,7 +9,7 @@ public abstract class Entity extends Animated {
     protected final int HEIGHT = this.spriteSheet.getSPRITE_HEIGHT();
     protected double speed = 0.5;
     protected Vector2D position;
-    protected Vector2D lastMovement;
+    protected Vector2D lastMovement = new Vector2D(0,0);
 
 
     public Entity(SpriteSheet spriteSheet, Vector2D position) {
