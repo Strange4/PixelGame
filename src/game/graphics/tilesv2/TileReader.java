25cc460749003e54a77d31e2238458dc103e121e
Mauricio Murillo <mauriciodelcipote@hotmail.com>
2021-11-18 22:46:49 -0500

    made the loadLayers method by tileCoordinates

--- src/game/graphics/tilesv2/TileReader.java
+++ src/game/graphics/tilesv2/TileReader.java
@@ -8,6 +8,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -20,9 +21,14 @@ public class TileReader {
     private int mapWidth;
     private int mapHeight;
     private ArrayList<TileSet> allTileSets;
+    private int[][] tileCoordinates;
+
+    //TODO: render to a bufferedimage in this file using the tilesets and the coordinates
+
     public TileReader(String path){
         loadXMLFile(path);
         loadTileSets();
+        loadLayers();
     }
 
     private void loadXMLFile(String path){
@@ -33,6 +39,12 @@ public class TileReader {
         } catch (ParserConfigurationException | URISyntaxException | IOException | SAXException e) {
             e.printStackTrace();
         }
+        Element mapInfo = (Element) this.xmlMap.getElementsByTagName("map").item(0);
+        this.tileWidth = Integer.parseInt(mapInfo.getAttribute("tileheight"));
+        this.tileHeight = Integer.parseInt(mapInfo.getAttribute("tileheight"));
+        this.mapHeight = Integer.parseInt(mapInfo.getAttribute("height"));
+        this.mapWidth = Integer.parseInt(mapInfo.getAttribute("width"));
+        this.tileCoordinates = new int[mapWidth][mapHeight];
     }
 
     private void loadTileSets(){
@@ -57,6 +69,24 @@ public class TileReader {
 
     // TODO: do this
     private void loadLayers(){
+        NodeList layers = this.xmlMap.getElementsByTagName("layer");
+        for(int i=0;i<layers.getLength();i++){
+            int[] tileIds = new int[this.mapWidth * this.mapHeight];
+            String data = ((Element)layers.item(i)).getElementsByTagName("data").item(0).getTextContent();
+            String[] tileNumbers = data.split(",");
+            for(int e=0;e<tileNumbers.length;e++){
+                tileIds[e] = Integer.parseInt(tileNumbers[e]);
+            }
+            for(int tileX=0;tileX<this.mapWidth;tileX++){
+                for(int tileY=0;tileY<this.mapHeight;tileY++){
+                    tileCoordinates[tileX][tileY] = tileIds[tileX + (tileY * mapWidth)];
+                }
+            }
+        }
+    }
+
+    //TODO: only render the tiles that are non-zero
+    public void render(Graphics2D graphics2D){
 
     }
 
