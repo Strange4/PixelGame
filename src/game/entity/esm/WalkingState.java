157150bd9042f7f3cd32a11f88f70e253e4fa53d
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-11-22 14:30:25 -0500

    working on ESM and updating animation appropriatly when player is standing/walking

--- src/game/entity/esm/WalkingState.java
+++ src/game/entity/esm/WalkingState.java
@@ -1,15 +1,2 @@
-package game.entity.esm;
-
-import game.entity.Entity;
-
-public class WalkingState extends OnGroundState{
-
-    @Override
-    public void update(Entity entity) {
-        if(entity.getState() == EntityState.STATE_WALKING){
-
-        } else {
-            super.update(entity);
-        }
-    }
+package game.entity.esm;public class WalkingState {
 }
---
+++ src/game/entity/types/Player.java
@@ -0,0 +1,15 @@
+package game.entity;
+
+import game.graphics.SpriteSheet;
+import game.util.Vector2D;
+
+public class Player extends Entity {
+
+    public Player(SpriteSheet spriteSheet, Vector2D position, int maxFrames) {
+        super(spriteSheet, position, maxFrames);
+    }
+
+    public void update(){
+
+    }
+}
