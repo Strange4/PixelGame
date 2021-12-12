5cf831d01f3a13a231460ec1a66537604b5c289f
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-12-11 21:40:46 -0500

    comments on keyboard

--- src/game/keyboard/KeyHandler.java
+++ src/game/keyboard/KeyHandler.java
@@ -5,6 +5,7 @@ import game.util.Vector2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+// Singleton
 public final class KeyHandler implements KeyListener {
     private static final KeyHandler INSTANCE = new KeyHandler();
     private Keyboard keyboard;
@@ -19,20 +20,34 @@ public final class KeyHandler implements KeyListener {
         this.ah = new AbilityHandler(keyboard);
     }
 
+    /**
+     * Returns the last pressed key on the keyboard
+     * @return Key last pressed key on the keyboard
+     */
     public Key getLastKey(){
         return this.keyboard.getLastPressedKey();
     }
 
+    /**
+     * Returns the KeyHandler instance of the singleton
+     * @return KeyHandler
+     */
     public static KeyHandler getInstance(){
         return INSTANCE;
     }
 
+    /**
+     * Returns the directional unit vector representing the movement of the player
+     * @return Vector2D directional vector representing the movement of the player
+     */
     public Vector2D getDirectionalVector(){
         return this.mh.getDirectional2DVector();
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+        // Not used but must be implemented
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
